1981c5670abac57f52de2fe08e4a520008624b64
eediallo <elhhabdouldiallo@gmail.com>
2025-06-04 13:57:58 +0100

    add function to replace both duplicate and unique chars

--- Sprint-1/prep/worked-exmaple.js
+++ Sprint-1/prep/worked-exmaple.js
@@ -1,8 +1,29 @@
-
 function replaceUniqueChars(str) {
-    const uniqChars = Array.from(new Set(str)).join('')
-    return uniqChars.replaceAll(/./g, '~')
+  const uniqChars = Array.from(new Set(str)).join("");
+  return uniqChars.replaceAll(/./g, "~");
 }
 
+function replaceChars(str) {
+  const lowerStr = str.toLowerCase();
+  // Count occurrences of each character
+  const counts = {};
+  for (const char of lowerStr) {
+    counts[char] = (counts[char] || 0) + 1;
+  }
 
-console.log(replaceUniqueChars('eellhhaadjj'))
+  // Replace repeated characters with 'a', keep unique as is
+  let result = "";
+  for (const char of lowerStr) {
+    if (counts[char] > 1) {
+      result += "*";
+    } else {
+      result += "1";
+    }
+  }
+
+  return result;
+}
+
+console.log(replaceChars("recede"));
+console.log(replaceChars("Success"));
+console.log(replaceChars("11*2"));
